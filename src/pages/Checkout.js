d6f612f0320a57d82c5836dfd8deb78761bd7fd3
ArielScc <lf0ariel@gmail.com>
2021-06-20 19:50:51 -0400

    feat: Page-Checkout, Agregado responsividad

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -11,13 +11,18 @@ import {
   HStack,
   Text,
   chakra,
-  Avatar,
   Badge,
   Button,
   Heading,
   Divider,
+  Flex,
+  Image,
+  IconButton,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { ArrowForwardIcon, DeleteIcon } from '@chakra-ui/icons';
+import { VscSymbolNumeric } from 'react-icons/vsc';
+import { IoPricetagsOutline } from 'react-icons/io5';
 import useStylesDark from '../utils/darkmode';
 import initialstate from '../initalstate';
 
@@ -28,48 +33,97 @@ const Checkout = () => {
 
   return (
     <>
-      <HStack w="7xl" bg={bg} spacing="4" p="4" rounded="lg">
-        <Table variant="simple" w="70%">
+      <Flex
+        direction={['column', 'column', 'row']}
+        maxW={['sm', '2xl', '7xl']}
+        bg={bg}
+        p="4"
+        rounded="lg"
+      >
+        <Table variant="simple" w={['full', 'full', '70%']}>
           <chakra.caption fontSize="xl" fontWeight="bold" mb="4">
             Checkout List
           </chakra.caption>
           <Thead>
             <Tr>
               <Th>Product</Th>
-              <Th>Status</Th>
-              <Th isNumeric>Quantity</Th>
-              <Th isNumeric>Price</Th>
+              <Th display={['none', 'none', 'table-cell']}>Status</Th>
+              <Th isNumeric>
+                <HStack>
+                  <VscSymbolNumeric />
+                  <Text
+                    display={{
+                      base: 'none',
+                      lg: 'inherit',
+                    }}
+                  >
+                    Quantity
+                  </Text>
+                </HStack>
+              </Th>
+
+              <Th isNumeric>
+                <HStack>
+                  <IoPricetagsOutline />
+                  <Text
+                    display={{
+                      base: 'none',
+                      lg: 'inherit',
+                    }}
+                  >
+                    Price
+                  </Text>
+                </HStack>
+              </Th>
+
               <Th>Action</Th>
             </Tr>
           </Thead>
           <Tbody>
             {cart.map((product) => (
               <Tr key={product.id}>
-                <Td display="flex">
-                  <Avatar
-                    size="lg"
-                    name={product.title}
+                <Td
+                  display="flex"
+                  flexDirection={['column', 'row']}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Image
+                    borderRadius="full"
+                    boxSize={['50px', '75px']}
+                    bg={useColorModeValue('gray.200', 'gray.700')}
+                    objectFit="contain"
                     src={product.image}
-                    bg="gray.200"
+                    alt={product.title}
+                    mr={['none', 'none', '2']}
                   />
-                  <Box ml="3">
+                  <Box>
                     <chakra.h4 color={textColor} fontWeight="bold">
                       {product.title}
                     </chakra.h4>
-                    <Text color={textColor} pt="1">
+                    <Text
+                      display={['none', 'contents']}
+                      color={textColor}
+                      pt="1"
+                    >
                       {product.description}
                     </Text>
                   </Box>
                 </Td>
-                <Td>
-                  <Badge ml="1" colorScheme="green">
-                    New
-                  </Badge>
+                <Td display={['none', 'none', 'table-cell']}>
+                  <Badge colorScheme="green">New</Badge>
                 </Td>
                 <Td isNumeric>1</Td>
                 <Td isNumeric>$ {product.price}</Td>
                 <Td>
+                  <IconButton
+                    display={['initial', 'initial', 'none']}
+                    icon={<DeleteIcon />}
+                    variant="solid"
+                    colorScheme="red"
+                  />
                   <Button
+                    display={['none', 'none', 'initial']}
                     leftIcon={<DeleteIcon />}
                     variant="solid"
                     colorScheme="red"
@@ -82,18 +136,35 @@ const Checkout = () => {
             ))}
           </Tbody>
         </Table>
-        <VStack w="30%" p="6" spacing="3" alignSelf="flex-start">
+        <VStack
+          alignSelf="flex-start"
+          p="6"
+          spacing="3"
+          w={['full', 'full', '30%']}
+          border="1px"
+          borderColor="gray.200"
+          rounded="xl"
+          boxShadow="md"
+          ml={{ base: '0', md: '6' }}
+          my={{ base: '6', md: '0' }}
+          color={textColor}
+        >
           <Heading as="h3" fontSize="xl">
             Details
           </Heading>
           <Divider />
-          <HStack justify="space-between" w="full">
-            <Text fontWeight="bold">Total Quantity</Text>
-            <Text>2</Text>
+          <HStack justify="space-between" w="full" fontWeight="bold">
+            <Text>Total Quantity</Text>
+            <Text fontSize="xl"> 2</Text>
           </HStack>
           <Divider />
-          <HStack justify="space-between" w="full">
-            <Text fontWeight="bold">Total Price</Text>
+          <HStack
+            justify="space-between"
+            w="full"
+            fontWeight="bold"
+            fontSize="2xl"
+          >
+            <Text>Total</Text>
             <Text>$35</Text>
           </HStack>
           <Button
@@ -106,7 +177,7 @@ const Checkout = () => {
             Continue Order
           </Button>
         </VStack>
-      </HStack>
+      </Flex>
     </>
   );
 };
