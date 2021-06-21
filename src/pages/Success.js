2bebfd40ea733a8def39f01ddb5a599921b53476
ArielScc <lf0ariel@gmail.com>
2021-06-21 00:22:37 -0400

    Agregado stilos de Success

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Text, VStack, AspectRatio, Box, Heading } from '@chakra-ui/react';
+import useStylesDark from '../utils/darkmode';
 
-const Success = () => (
-  <div>
-    <Text>Success</Text>
-  </div>
-);
+const Success = () => {
+  const { bg, textColor } = useStylesDark();
+  return (
+    <>
+      <VStack as="form" w="7xl" bg={bg} p="8" color={textColor}>
+        <Heading>Punchase Confirmation</Heading>
+        <Text>Gracias por Realizar tu compra</Text>
+        <Text>Tu pedido llegara en 3 días a tu dirección</Text>
+        <Box w="full">
+          <AspectRatio ratio={4 / 1}>
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng"
+              alt="demo"
+            />
+          </AspectRatio>
+        </Box>
+      </VStack>
+    </>
+  );
+};
 
 export default Success;
